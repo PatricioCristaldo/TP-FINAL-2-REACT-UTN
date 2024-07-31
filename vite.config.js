3dda402c9f813e59508b6e96935b5e4f7c7a2ecd
Patricio <patricio_cristaldo@outlook.com>
2024-07-31 01:56:29 -0300

    /

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   
-  base: '/tp-final-2-react-utn/', // Asegúrate de que esto apunte al nombre de tu repositorio
+  base: '/tp-final-2-react-utn', // Asegúrate de que esto apunte al nombre de tu repositorio
   
   server: {
     historyApiFallback: true, // Añadir esta línea para manejar el enrutamiento del lado del cliente
